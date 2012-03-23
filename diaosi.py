68e70dac59e122cb2b779b4456ba2fc72e871086
ccnusjy <ccnusjy@gmail.com>
2012-03-23 14:01:33 +0000

    spelling correction

--- diaosi.py
+++ diaosi.py
@@ -1,5 +1,8 @@
 import sdict
 import os
+import string
+
+alphabets='abcdefghijklmnopqrstuvwxyz'
 
 def current_file_directory():
 			import os, sys, inspect
@@ -11,6 +14,15 @@ def current_file_directory():
 				caller_file = inspect.stack()[1][1]     # function caller's filename
 				return os.path.abspath(os.path.dirname(caller_file))# return function caller's file's directory
 
+def edits1(W):
+	N = len(W)
+	splits = [(W[:i],W[i:]) for i in xrange(N)]
+	deletes = [ x+y[1:] for x,y in splits if y]
+	trans = [a + b[1] + b[0] +b[2:] for a,b in splits if len(b)>1]
+	replaces = [a + c + b[1:] for a,b in splits for c in alphabets if b]
+	inserts =  [a + c + b for a,b in splits for c in alphabets]
+	return set(deletes+trans+replaces+inserts)
+	
 index = sdict.Dict()
 _localDir=current_file_directory()
 
@@ -39,3 +51,8 @@ while True:
 	for w in rel_words:
 		print w,index[w]
 
+	if len(rel_words)==0:
+		cor_words = edits1(word)
+		for w in cor_words:
+			if w in index:
+				print '? ',w
